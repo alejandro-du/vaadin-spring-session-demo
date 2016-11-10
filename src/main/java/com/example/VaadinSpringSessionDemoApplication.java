870195aca7706625106ea8a9ba478f71e66c0ebb
Alejandro Duarte <alejandro@vaadin.com>
2016-11-10 18:00:11 +0200

    Use spring-session-redis Vaadin add-on insted of the 'dirty hack'

--- src/main/java/com/example/VaadinSpringSessionDemoApplication.java
+++ src/main/java/com/example/VaadinSpringSessionDemoApplication.java
@@ -2,14 +2,25 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.vaadin.spring.session.redis.VaadinSessionRewriteFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableRedisHttpSession
+// @ServletComponentScan("org.vaadin.spring.session.redis") // alternative to defining the vaadinSessionRewriteFilter bean
 public class VaadinSpringSessionDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VaadinSpringSessionDemoApplication.class, args);
+    }
+
+    @Bean
+    public Filter vaadinSessionRewriteFilter() {
+        return new VaadinSessionRewriteFilter();
     }
 
 }
